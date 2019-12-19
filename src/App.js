f5bbba5646112365379c3271b8b1a5b2bc879fc5
wencaizhang <1052642137@qq.com>
2019-12-19 09:46:35 +0800

    chore: add antd ui framework

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from "antd";
+import "antd/dist/antd.css";
 
 function App() {
   return (
     <div className="App">
-     
+     <Button>antd button</Button>
     </div>
   );
 }
