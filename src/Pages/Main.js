954d45cc52e864c072f888d956e50212b647e33b
wencaizhang <1052642137@qq.com>
2019-12-19 10:05:21 +0800

    chore: add react router

---
+++ src/Pages/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
+
+function Main () {
+  return (
+    <Router>
+      <Route path="/" exact component={Home} />
+      <Route path="/login/" exact component={Login} />
+    </Router>
+  )
+}
+
+export default Main;
