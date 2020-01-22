04f025ad0b0a35acdd1346ec24b38ba4e9bde60d
wencaizhang <1052642137@qq.com>
2020-01-22 15:07:11 +0800

    add redirect

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Redirect } from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login";
 
 function Main () {
   return (
     <Router>
+      <Redirect to='/posts/list'></Redirect>
       <Route path="/posts/" component={Home} />
       <Route path="/login/" component={Login} />
     </Router>
